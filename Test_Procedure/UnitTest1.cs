02aa6b22383271867ff94546f696e49845c87db0
Ovod1 <1ovod1@mail.ru>
2019-09-04 17:22:18 +0300

    Вынес функциональность в отдельный метод, сделал драйвер обобщённым

--- Test_Procedure/UnitTest1.cs
+++ Test_Procedure/UnitTest1.cs
@@ -10,23 +10,28 @@ namespace Test_Procedure
     //[TestClass]
     //public class UnitTest1
     //{
-    //    FirefoxDriver firefox;
-
-    //    [TestMethod]
-    //    public void TestMethod1()
+    //    FirefoxDriver firefox;   
+    //    public void SearchElements(string[] paths, string firstLink, string pathElements, int rightAmount)
     //    {
     //        firefox = new FirefoxDriver();
-    //        firefox.Navigate().GoToUrl("https://careers.veeam.com/");
+    //        firefox.Navigate().GoToUrl(firstLink);
     //        firefox.Manage().Window.Maximize();
-    //        firefox.FindElement(By.XPath("//div[2]/dd/div/span")).Click();
-    //        firefox.FindElement(By.XPath("//div/div[2]/span[17]")).Click();
-    //        firefox.FindElement(By.XPath("//div/div[5]/div/span")).Click();
-    //        firefox.FindElement(By.XPath("//fieldset/label[5]")).Click();
-    //        //firefox.FindElement(By.XPath("//div[2]/div/div/a")).Click(); -закрытие окна выбора языка
-    //    firefox.FindElement(By.XPath("//section[3]/div/div[2]/div/a")).Click();
+    //        foreach (string path in paths)
+    //        {
+    //            firefox.FindElement(By.XPath(path)).Click();
+    //        }
     //        Thread.Sleep(6000);
-    //        int i = firefox.FindElements(By.XPath("//div/a/h4")).Count;
-    //        Assert.AreEqual(28, i);
+    //        int testAmount = firefox.FindElements(By.XPath(pathElements)).Count;
+    //        Assert.AreEqual(rightAmount, testAmount);
+    //    }
+    //    [TestMethod]
+    //    public void StartSearch()
+    //    {
+    //        string[] paths = { "//div[2]/dd/div/span", "//div/div[2]/span[17]", "//div/div[5]/div/span", "//fieldset/label[5]", "//div[2]/div/div/a", "//section[3]/div/div[2]/div/a"};
+    //        var firstLink = "https://careers.veeam.com/";
+    //        var pathElements = "//div/a/h4";
+    //        var rightAmount = 28;
+    //        SearchElements(paths, firstLink, pathElements, rightAmount);
     //    }
     //    [TestCleanup]
     //    public void TearDown()
@@ -34,36 +39,36 @@ namespace Test_Procedure
     //        firefox.Quit();
     //    }
     //}
+    public class Search<T> where T : FirefoxDriver, new()
+    {
+
+        T driver;
+        public void SearchElements(string[] paths, string firstLink, string pathElements, int rightAmount)
+        {
+            driver = new T();
+            driver.Navigate().GoToUrl(firstLink);
+            driver.Manage().Window.Maximize();
+            foreach (string path in paths)
+            {
+                driver.FindElement(By.XPath(path)).Click();
+            }
+            Thread.Sleep(4000);
+            int testAmount = driver.FindElements(By.XPath(pathElements)).Count;
+            Assert.AreEqual(rightAmount, testAmount);
+        }    
+    }
     [TestClass]
     public class UnitTest1
     {
-        FirefoxDriver firefox;   
-        public void SearchElements(string[] paths, string firstLink, string pathElements, int rightAmount)
-        {
-            firefox = new FirefoxDriver();
-            firefox.Navigate().GoToUrl(firstLink);
-            firefox.Manage().Window.Maximize();
-            foreach (string path in paths)
-            {
-                firefox.FindElement(By.XPath(path)).Click();
-            }
-            Thread.Sleep(6000);
-            int testAmount = firefox.FindElements(By.XPath(pathElements)).Count;
-            Assert.AreEqual(rightAmount, testAmount);
-        }
         [TestMethod]
         public void StartSearch()
         {
-            string[] paths = { "//div[2]/dd/div/span", "//div/div[2]/span[17]", "//div/div[5]/div/span", "//fieldset/label[5]", "//div[2]/div/div/a", "//section[3]/div/div[2]/div/a"};
+            var search1 = new Search<FirefoxDriver> { };
+            string[] paths = { "//div[2]/dd/div/span", "//div/div[2]/span[17]", "//div/div[5]/div/span", "//fieldset/label[5]", "//div[2]/div/div/a", "//section[3]/div/div[2]/div/a" };
             var firstLink = "https://careers.veeam.com/";
             var pathElements = "//div/a/h4";
             var rightAmount = 28;
-            SearchElements(paths, firstLink, pathElements, rightAmount);
-        }
-        [TestCleanup]
-        public void TearDown()
-        {
-            firefox.Quit();
+            search1.SearchElements(paths, firstLink, pathElements, rightAmount);          
         }
     }
 }
